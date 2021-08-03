511b3bf41eb552ff3cbb2a596618d0f5a2717699
Abdifatah <zafex188@hotmail.com>
2021-08-03 18:09:31 +0200

    fix: error handling, and data assigning

---
+++ src/component/ShowWeather.js
@@ -0,0 +1,90 @@
+import React, {Component, useEffect, useState} from "react";
+import Axios from 'axios'
+import {GoSearch} from 'react-icons/go';
+
+import styles from './ShowWeather.module.css'
+import cloudSvg from '../images/cloud.svg'
+
+
+function ShowWeather() {
+
+  const [city, setCity] = useState("")
+  const [weather, setWeather] = useState([])
+  const [error, setError] = useState(false)
+  const [active, setActive] = useState(false)
+
+  const searchCity = async (e) => {
+    e.preventDefault()
+    try {
+      const weatherResponse = await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.React_App_APIKEY}&lang=en`)
+      setError(null)
+      setWeather(weatherResponse.data)
+    } catch (e) {
+      setError("City not found")
+      console.log("City not found")
+      setCity("")
+      console.log(e)
+    }
+  }
+
+  function errorHandler() {
+    return <div className={styles.error} role="alert">
+      {error}
+    </div>
+  }
+
+  function showWeatherCard(){
+    setActive(!active)
+  }
+
+  return (
+    <div>
+      <div className={styles.wrap}>
+        <form onSubmit={searchCity} className={styles.search}>
+          <input
+            className={styles.searchTerm}
+            type="text"
+            name="weatherCity"
+            placeholder="Search City"
+            onChange={(e) => setCity(e.target.value)}
+            required
+          />
+          <button type="submit" className={styles.searchButton} onClick={() => {
+
+          }}>
+            <a><GoSearch/></a>
+          </button>
+        </form>
+        <div>{errorHandler()}</div>
+      </div>
+      <div className={styles.weather_container}>
+        <div className={styles.inner_container}>
+          <div className={styles.location}>
+            <div><i className="fas fa-map-marker-alt"/></div>
+            {weather && weather.sys && <h3>{weather.name}, {weather.sys.country}</h3>}
+
+          </div>
+          <div className={styles.weather_icon}>
+
+            {weather.weather &&
+            <img src={`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt="cloud image"/>}
+          </div>
+          <div className={styles.cloud_status}>
+
+            {weather.weather && <h2>{weather.weather[0].main}</h2>}
+          </div>
+          <div className={styles.temp}>
+            {weather.main && <h1>{weather.main.temp}&deg;C</h1>}
+          </div>
+          <div className={styles.wind_hum}>
+            {weather.wind && <span><i className="fas fa-wind"/>WS: {weather.wind.speed} km/s</span>}
+            {weather.main && <span><i className="fas fa-tint"/>Humidity: {weather.main.humidity}%</span>}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default ShowWeather
